863ee4c07c73a6ec096fbea6cd648e866d24f910
David Colatti <davidcolatti@gmail.com>
2020-06-25 11:26:35 -0400

    feat: database connection

--- src/config/database.js
+++ src/config/database.js
@@ -1,8 +1,8 @@
 module.exports = {
   host: "127.0.0.1",
-  username: "docker",
-  password: "docker",
-  database: "nodeauth",
+  username: "davidcolatti",
+  password: "davidcolatti",
+  database: "davidcolatti",
   dialect: "postgres",
   operatorsAliases: false,
   logging: false,
