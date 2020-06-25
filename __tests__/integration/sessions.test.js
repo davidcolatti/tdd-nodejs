ea3479604fe88ee52f60f82dda7ccb5d64bb1008
David Colatti <davidcolatti@gmail.com>
2020-06-25 14:00:13 -0400

    feat: test for password encryption

--- __tests__/integration/sessions.test.js
+++ __tests__/integration/sessions.test.js
@@ -2,13 +2,18 @@ const request = require("supertest");
 
 const app = require("../../src/app");
 const { User } = require("../../src/app/models");
+const truncate = require("../utils/truncate");
 
 describe("Authentication", () => {
+  beforeEach(async () => {
+    await truncate();
+  });
+
   it("should authenticate with valid credentials", async () => {
     const user = await User.create({
       name: "Jimmy",
       email: "jimmycolatti@gmail.com",
-      password_hash: "123123",
+      password: "123123",
     });
 
     const response = await request(app).post("/sessions").send({
