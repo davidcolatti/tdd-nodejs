d9d5643fd7cf99f515cb631fe19c7d1e1a01f711
David Colatti <davidcolatti@gmail.com>
2020-06-25 12:05:42 -0400

    feat: test for authentication

--- src/routes.js
+++ src/routes.js
@@ -1,5 +1,7 @@
 const routes = require("express").Router();
 
-// defined routes
+const SessionController = require("./app/controllers/SessionController");
+
+routes.post("/sessions", SessionController.store);
 
 module.exports = routes;
